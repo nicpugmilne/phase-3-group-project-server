83e9b79e6aba518032c50cf3ee4705aa949008cb
Nicola Milne <nicolalouisemilne@gmail.com>
2022-07-13 14:32:16 -0600

    created new endpoint for checking the current order

--- app/models/order.rb
+++ app/models/order.rb
@@ -2,7 +2,11 @@ class Order < ActiveRecord::Base
     belongs_to :restaurant
     has_many :ordered_items
     
+    def self.get_open_order
+        Order.where("is_completed == ?", false).first
+    end
+
     def self.get_open_order_items
-        Order.where("is_completed == ?", false).first.ordered_items
+        Order.get_open_order.ordered_items
     end
 end 
